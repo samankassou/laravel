76c98fa9e440ab5823222865d51f9d15cf4a311f
Samankassou <samankassoufoulla@gmail.com>
2020-10-19 02:38:52 +0100

    first commit

---
+++ app/Http/Controllers/Properties.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Properties extends Controller
+{
+    public function index()
+    {
+        return 'Liste des biens';
+    }
+}
